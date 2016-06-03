e6bee5340c4a86d5d981dcd628b3d8ed9eae1611
hushuaibing <hushuaibing@lbesec.com>
2016-06-03 11:06:34 +0800

    完善demo

---
+++ app/src/main/java/com/moshangjian/switchview/FullImageView.java
@@ -0,0 +1,28 @@
+package com.moshangjian.switchview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class FullImageView extends ImageView {
+    public FullImageView(Context context) {
+        super(context);
+    }
+
+    public FullImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FullImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        Log.d("hb","width = " + widthSize + " height = " + heightSize);
+        setMeasuredDimension(widthSize,widthSize);
+    }
+}
--- app/src/main/java/com/moshangjian/switchview/MainActivity.java
+++ app/src/main/java/com/moshangjian/switchview/MainActivity.java
@@ -2,12 +2,34 @@ package com.moshangjian.switchview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] imageResource = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4,
+            R.drawable.image5, R.drawable.image6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SwitchView switchView = (SwitchView) findViewById(R.id.main_switch);
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) (width * 0.6 * 0.5));
+        switchView.setLayoutParams(layoutParams);
+
+        for (int i = 0; i < 5; i++) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_banner, null, false);
+            ImageView imageView = (ImageView) view.findViewById(R.id.item_banner_img);
+            imageView.setImageResource(imageResource[i]);
+            switchView.addView(view);
+        }
     }
 }
